fe5c53be0654a7a8189e7c757534674e1a9b1fc2
Giovanni Guarnieri <giovanniguarnieri@id.uff.br>
2017-12-05 15:49:25 -0200

    Condições periodicas adicionadas

--- media.py
+++ media.py
@@ -5,25 +5,28 @@ import numpy as np
 import sys
 
 n=200
-aux=np.zeros((n,n), float)
-mask=np.zeros((n,n), float)
+aux=np.zeros((n,n+2), float)
+mask=np.zeros((n,n+2), float)
 
 
 for i in xrange(n):
  mask.itemset((0,i), 1)
  aux.itemset((0,i), 100)
 for i in xrange(n-1,n/2,-1):
- mask.itemset((i,n/2), 1)
+ mask.itemset((i,n/2+1), 1)
 maior=0.1
 guarda=[]
 ii=0
-while(ii<200):
+while(ii<4000):
  for i in xrange(n-1):
-  for j in xrange(n-1):
+  for j in xrange(1,n+1):
    if(mask.item(i,j)!=1):
     aux2=aux.item(i,j)
     aux.itemset((i,j), (aux.item(i+1,j)+aux.item(i-1,j)+aux.item(i,j+1)+aux.item(i,j-1))/4)
-    guarda.append(abs(aux2-aux.item(i,j)))
+#    guarda.append(abs(aux2-aux.item(i,j)))
+ for j in xrange(n): 
+  aux.itemset((j,0),aux.item(j,n))
+  aux.itemset((j,n+1),aux.item(j,1))
  ii=ii+1
 
 for i in xrange(n-1,-1,-1):
